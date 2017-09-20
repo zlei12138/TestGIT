32bc6248a43b3aac40bd9d325f264cd59fde383f
chongerfei123 <779545095@qq.com>
2017-09-20 14:30:44 +0800

    6

--- app/src/main/java/st/zlei/com/testgit/MainActivity.java
+++ app/src/main/java/st/zlei/com/testgit/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        32duhjwec
+//；ldxasca
+        //带卡哦亲fijfd
     }
 }
